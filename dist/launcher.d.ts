711ec5745bfc57976b71433b6ee3c5a4f8115cf7
utf94 <utf94@users.noreply.github.com>
2026-05-16 15:21:14 +0500

    Ignore dist and node_modules; stop tracking dist outputs

--- dist/launcher.d.ts
+++
@@ -1,8 +0,0 @@
-import { type PackageLauncherPanel, type RequestContext } from './contracts.js';
-export declare function createConnectingmatrixFileStubLauncher(context?: RequestContext): PackageLauncherPanel;
-export declare const createStubLauncher: typeof createConnectingmatrixFileStubLauncher;
-export declare const Launcher: {
-    open: typeof createConnectingmatrixFileStubLauncher;
-    mode: "stub";
-};
-export declare const launcher: typeof createConnectingmatrixFileStubLauncher;
